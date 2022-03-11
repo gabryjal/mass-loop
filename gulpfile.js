27841fdb9b5a93109a8aaa190597995f8ade2dc1
gracjan <gracjan.piwiec@osworkshop.com>
2022-03-11 17:44:51 +0100

    some global styles, and product block

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,25 @@ const browserSync = require('browser-sync').create();
 const { src, dest } = require('gulp');
 
 function style() {
-  return gulp.src('./src/scss/**/*.scss')
+  return gulp.src('./src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/stylesheets'))
     .pipe(browserSync.stream());
 }
 
-function copy() {
+function indexCopy() {
+  return src('src/index.html')
+    .pipe(dest('public/index.html'))
+}
+
+function imageCopy() {
   return src('src/images/*')
     .pipe(dest('public/images'))
+}
+
+function fontsCopy() {
+  return src('src/fonts/*')
+    .pipe(dest('public/fonts'))
 }
 
 function watch() {
@@ -24,9 +34,9 @@ function watch() {
   gulp.watch('./src/scss/**/*.scss', style)
   gulp.watch('./public/*.html').on('change', browserSync.reload)
   gulp.watch('./js/**/*.js').on('change', browserSync.reload)
+  gulp.watch('src/images/*', imageCopy).on('change', browserSync.reload)
+  gulp.watch('src/fonts/*', fontsCopy).on('change', browserSync.reload)
+  // gulp.watch('src/index.html', indexCopy).on('change', browserSync.reload)
 }
 
-
-exports.style = style;
 exports.watch = watch;
-exports.copy = copy;

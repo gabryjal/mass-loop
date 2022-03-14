f52a148a7c14b7e98394b51e039e7d4db42bc4fe
gracjan <gracjan.piwiec@osworkshop.com>
2022-03-14 01:42:37 +0100

    finall changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,31 @@
+//Calculator
 let input1 = document.querySelector('#calculator-input-first');
 let input2 = document.querySelector('#calculator-input-second');
-input1.addEventListener('change', updateValue);
-input2.addEventListener('change', updateValue);
-//
-function updateValue() {
-  let A = document.querySelector('#calculator-input-first').value;
-  let B = document.querySelector('#calculator-input-second').value;
-  let result = parseFloat((A*B-A*B/5).toFixed(2));
-  let res = document.querySelector('.calculator__result-price');
-  res.innerHTML = `<span style="color: #FFB001">`+result+` zł</span>`
-}
+
+[input1, input2].forEach((e) => {
+  e.addEventListener('input', (e) => {
+    let A = input1.value;
+    let B = input2.value;
+    if (A === "") {
+      A = 20;
+      input1.classList.remove('calculator__value-input--highlight');
+    } else {
+      input1.classList.add('calculator__value-input--highlight');
+    }
+    if (B === "") {
+      B = 20;
+      input2.classList.remove('calculator__value-input--highlight');
+    }else {
+      input2.classList.add('calculator__value-input--highlight');
+    }
+    let res = document.querySelector('.calculator__result-price');
+    let result = (12*(A * B - A * B / 5)).toFixed(2);
+    res.innerHTML = `<span style="color: #FFB001">` + result + ` zł</span>`
+  });
+});
+
+//sticky header
+window.addEventListener("scroll", ()=> {
+  let header = document.querySelector(".header__fixed");
+  header.classList.toggle("header__fixed--sticky", window.scrollY > 0);
+})
